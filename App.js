85642e75f97ac1f03763220bcff49c4f0c83437d
Ahalya-ashokan <ahalyaashok10596@gmail.com>
2025-10-09 11:37:23 +0530

    JSX

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "h1 tag od child1"),
-    React.createElement("h2", {}, "h2 tag of child1"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "h1 tag of child2"),
-    React.createElement("h2", {}, "h2 tag of child2"),
-  ]),
-]);
+const heading=<h1 id="heading">JSX Heading</h1>
+// const heading= React.createElement("h1",{id:"heading"},"Heading")
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React using external js "
-// );
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+// React functional component below
+
+const Title = ()=> (<h2 className="title">
+    Title component</h2>)
+const HeadingComponent= ()=>(
+    <div>
+    <Title/>
+    <h1 className="header">JSX---- from funtional component</h1>
+    </div>)
+
+
+const root= ReactDOM.createRoot(document.getElementById("root"))
+root.render(<HeadingComponent/>)
+
+//  use JSX from here
